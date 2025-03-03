9e676d0e1366c01486de467ff107455a73db5489
VKreddy <kreddyv9@gmail.com>
2025-03-02 22:02:14 -0600

    Pushing Demo Proj to github

---
+++ Demo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vaibh
+ *
+ */
+module Demo {
+}
---
+++ Demo/src/test/A.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package test;
+
+
+/**
+ * @author vaibh
+ *
+ */
+public class A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+int x;
+if(args.length>0) {
+	
+	x=10;
+}
+else {
+	x =20;
+}
+
+
+System.out.println(" String Durga");
+A.main(1);
+
+		
+		
+	}
+	
+	private static void main(int i) {
+		// TODO Auto-generated method stub
+		System.out.println("INTEEEEE");
+		
+	}
+
+	public static void main(int[] argss) {
+		System.out.println("Integer Durga");
+		
+	}
+	
+	}
+
+
